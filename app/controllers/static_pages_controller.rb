a344afd83e289bdbcdc94050654b9e64217f5c17
Kevin Townsend <kevinatown@gmail.com>
2016-03-11 08:46:46 +0000

    done for the night

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -21,33 +21,42 @@ class StaticPagesController < ApplicationController
   	@hotels = ActiveRecord::Base.connection.execute("call usp_getCustomersByEmail('" + @userat.UserID + "')")
 
   	@arr_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "EveryDay"]
-  
     # pulling params from the page
-    # days_selected = params[:selected_days]
-    # cust_name = params[:CustomerName]
-    # cust_id = params[:CustomerID]
-    # s_date = params[:start_date]
-    # e_date = params[:end_date]
+   
   	# @hotels = hquery.fetch_all
+    # @phash = params.dup
   end
   
   def continue 
-    phash = params.clone   
-    redirect_to flashRefineSearchRateTypes_path(phash)      
+    # @phash = params.dup
+     @phash = { "days_selected" => params[:selected_days],
+      "cust_name" => params[:CustomerName],
+      "cust_id" => params[:CustomerID],
+      "s_date" => params[:start_date],
+      "e_date" => params[:end_date]            
+    }
+    redirect_to flashRefineSearchRateTypes_path(@phash)      
   end
 
 
   def flashRefineSearchRateTypes
+    # phash2 = phash
     @rateTypes = ActiveRecord::Base.connection.execute("CALL usp_GetFlashRateTypes")
   end
 
   def continue2 
-    phash = params.clone   
-    redirect_to flashRefineSearch_path(phash)      
+    # phash = params.clone   
+    redirect_to flashRefineSearch_path()      
   end
 
+  # Still an issue with retriving the hash
   def flashRefineSearch
+    # phash3 = phash2
 
+
+    @show_hotels = [1,2]  # ActiveRecord::Base.connection.execute("call usp_GetFlashHotelsForCustomer('" + @phash[:CustomerID] + "')")
+
+    @show_sites = [1,2] # ActiveRecord::Base.connection.execute("call usp_GetFlashSitesForCustomer('" + @phash[:CustomerID] + "')")
   end
 
   def continue3
